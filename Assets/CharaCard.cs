4959fb13ab8f253c7c92c86e68099a206b1d8c65
green-Desktop <appieosugood@gmail.com>
2024-06-28 02:54:16 +0800

    chara data show on panel work

---
+++ Assets/CharaCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class CharaCard : MonoBehaviour
+    {
+        public TMP_Text nameText;
+
+        public void SetName(string name)
+        {
+            nameText.text = name;
+        }
+    }
+}
--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using DefaultNamespace;
+using Player.save;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
@@ -6,6 +9,7 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public Canvas canvas;
+    private List<CharaData> _charaStorage;
     private GameObject _charaStoragePanel;
 
     private void Start()
@@ -14,6 +18,80 @@ public class UIManager : MonoBehaviour
         var b = canvas.transform.Find("Body/CharaStorageButton").GetComponent<Button>();
         b.onClick.AddListener(CharaStorageButtonClick());
         _charaStoragePanel = canvas.transform.Find("Body/CharaStoragePanel").GameObject();
+        _charaStorage = new List<CharaData>();
+        _charaStorage.Add(new CharaData
+        {
+            name = "Chara1",
+            battleData = new BattleData
+            {
+                atk = 10,
+                def = 10,
+                cri = 10,
+                spd = 10,
+                dodge = 10
+            },
+            energy = new MaxableNumber
+            {
+                now = 100,
+                max = 100
+            },
+            hp = new MaxableNumber
+            {
+                now = 100,
+                max = 100
+            },
+            hunger = new MaxableNumber
+            {
+                now = 100,
+                max = 100
+            },
+            mood = new MaxableNumber
+            {
+                now = 100,
+                max = 100
+            }
+        });
+        _charaStorage.Add(new CharaData
+        {
+            name = "Chara2",
+            battleData = new BattleData
+            {
+                atk = 20,
+                def = 20,
+                cri = 20,
+                spd = 20,
+                dodge = 20
+            },
+            energy = new MaxableNumber
+            {
+                now = 100,
+                max = 100
+            },
+            hp = new MaxableNumber
+            {
+                now = 100,
+                max = 100
+            },
+            hunger = new MaxableNumber
+            {
+                now = 100,
+                max = 100
+            },
+            mood = new MaxableNumber
+            {
+                now = 100,
+                max = 100
+            }
+        });
+
+        var charaStorageView = canvas.transform.Find("Body/CharaStoragePanel/View/Viewport/Content").GameObject();
+        var charaCardPrefab = Resources.Load<GameObject>("UI/CharaCard");
+        foreach (var data in _charaStorage)
+        {
+            var go = Instantiate(charaCardPrefab, charaStorageView.transform);
+            go.GetComponent<CharaCard>().SetName(data.name);
+        }
+        
     }
 
     // Update is called once per frame
@@ -23,9 +101,6 @@ public class UIManager : MonoBehaviour
 
     private UnityAction CharaStorageButtonClick()
     {
-        return () =>
-        {
-            _charaStoragePanel.SetActive(!_charaStoragePanel.activeSelf);
-        };
+        return () => { _charaStoragePanel.SetActive(!_charaStoragePanel.activeSelf); };
     }
 }
